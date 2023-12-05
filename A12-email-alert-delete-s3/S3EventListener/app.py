4a3e7e28805917c62597fea101201c9a44ce3f3b
shuchitha <shuchitham@gmail.com>
2023-12-05 10:16:31 +0530

    A12 Email alert

---
+++ A12-email-alert-delete-s3/S3EventListener/app.py
@@ -0,0 +1,41 @@
+import boto3
+import os
+from botocore.exceptions import BotoCoreError, ClientError
+
+# initialize ses client, get sender n recipient email
+ses_client = boto3.client('ses', region_name=os.getenv('REGION'))
+sender = os.getenv('SENDER_EMAIL')
+recipient = os.getenv('RECIPIENT_EMAIL')
+
+def lambda_handler(event, context):
+   #for each record check for delete  
+   for record in event['Records']:
+       if record['eventName'] == 'ObjectRemoved:Delete':
+           bucket_name = record['s3']['bucket']['name']
+           object_key = record['s3']['object']['key']
+           #construct email    
+           subject = f'File Deleted: {object_key}'
+           body = f'A file was deleted from the bucket {bucket_name}. The deleted file was {object_key}.'
+           
+           try:
+               # send mail using ses
+               response = ses_client.send_email(
+                  Source=sender,
+                  Destination={
+                      'ToAddresses': [recipient],
+                  },
+                  Message={
+                      'Subject': {
+                          'Data': subject,
+                      },
+                      'Body': {
+                          'Text': {
+                              'Data': body,
+                          }
+                      }
+                  }
+               )
+           except ClientError as e:
+               print(e.response['Error']['Message'])
+           else:
+               print("Email sent! Message Id:", response['MessageId'])
